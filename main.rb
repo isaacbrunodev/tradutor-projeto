d6e905321dd2ce997d8da9e4b2a7308d3acf6e16
Isaac <isaacbruno1996@gmail.com>
2023-08-24 09:35:28 -0300

    código completo

--- main.rb
+++ main.rb
@@ -19,5 +19,32 @@ class Tradutor
  
  private
 
+ def salvar_traducao(texto_original,texto_traduzido,idioma_destino)
+   Time.now.strftime("%Y-%m-%d_%H-%M") + ".text"
+File.open(nome_arquivo,"a")do |file|
+    file.puts ("Original(#{idioma_destino}): #{texto_original}")
+    file.puts("Tradução(#{idioma_destino}):#{texto_traduzido}")
+    file.puts('='*30)
+    end
+   end
+end
+
+# Uso da classe
+tradutor=Tradutor.new
+
+puts"Digite o texto"
+texto=gets.chomp
+
+puts"Digite o idioma do texto:"
+   idioma_origem=gets.chomp
+
+puts"Digite o idioma para o qual você deseja traduzir:"
+idioma_destino=gets.chomp
+
+texto_traduzido =tradutor.traduzir_texto(texto,idioma_origem,idioma_destino)
+puts "Texto Traduzido: #{texto_traduzido}"
+
+
+
  
 
